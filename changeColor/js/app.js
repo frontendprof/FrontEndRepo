3b8dd7d00eb8063e7e38c27e28bfb153300707e7
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-13 00:28:56 -0600

    Finalize

--- changeColor/js/app.js
+++ changeColor/js/app.js
@@ -1,12 +1,17 @@
 
 
-const colors=['red','green','blue','orange','cyan','magenta','brown','teal','pink','yellow'];
+const colors=['red','green','peacock','lilac','blue','orange','coral','cyan','magenta','brown','teal','pink','yellow','tan'];
 
 
-const btn=document.querySelector('#btn');
+const btn=document.querySelector('.btn');
 
 btn.addEventListener('click',changeColor);
 
 function changeColor(){
-    const random=Math.random()*colors.length;
+    const random=Math.floor(Math.random()*colors.length);
+
+    const body=document.body;
+    body.style.background=colors[random];
+    console.log(random);
+    
 }
