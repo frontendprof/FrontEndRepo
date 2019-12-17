00906b86f6a815b0178f2cd3568edeb7cf16948b
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-17 03:22:14 -0600

    New quotes added to quotes bank

--- quoteRandom/js/app.js
+++ quoteRandom/js/app.js
@@ -5,22 +5,34 @@
     const quotes = [
         {
           quote:
-            "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-          author: " Life"
+            "There is only one corner of the universe you can be certain of improving, and that's your own self.",
+          author: " Aldous Huxley"
         },
         {
           quote:
-            "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-          author: "John Kenneth Galbraith"
+            "A good traveler has no fixed plans and is not intent on arriving.",
+          author: "Lao Tzu"
         },
+        {
+          quote:
+            "We know what we are, but not what we may be",
+          author: "William Shakespeare"
+        },
+
+        {
+          quote:
+            "What we achieve inwardly will change outer reality.",
+          author: "Plutarch"
+        },
+
         {
           quote:
             "God save me from my friends. I can protect myself from my enemies.",
           author: "Claude Louis Hector de Villars "
         },
         {
-          quote: "The price of anything is the amount of life you exchange for it.",
-          author: "David Thoreau"
+          quote: "The most effective way to do it, is to do it.",
+          author: "Amelia Earhart"
         },
         {
           quote:
@@ -31,6 +43,23 @@
           quote:
             "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
           author: " Tyne Daly"
+        },
+        {
+          quote:
+            "The best way out is always through.",
+          author: "Robert Frost"
+        },
+
+        {
+          quote:
+            "A flaw in the human character is that everybody wants to build and nobody wants to do maintenance",
+          author: "Kurt Vonnegut"
+        },
+
+        {
+          quote:
+            "You can't wait for inspiration. You have to go after it with a club",
+          author: "Jack London"
         }
       ];
 
