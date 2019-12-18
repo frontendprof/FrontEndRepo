c049cd395073f5cce8e5a2e07f3dbaa1db7d9872
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-18 00:33:59 -0600

    Defining UI Variables

---
+++ TaskListPro/app.js
@@ -0,0 +1,6 @@
+// Defining UI Variables
+const taskList=document.querySelector(".collection");
+const taskInput=document.querySelector("#task");
+const filter=document.querySelector('#filter');
+const form=document.querySelector('#task-form');
+const clearBtn=document.querySelector(".btn");
