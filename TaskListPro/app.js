ac10dd8fd1f05cb9dd00cad6d3735ee84dc0f41c
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-18 00:53:27 -0600

    Adding value to taskinput field

--- TaskListPro/app.js
+++ TaskListPro/app.js
@@ -4,3 +4,46 @@ const taskInput=document.querySelector("#task");
 const filter=document.querySelector('#filter');
 const form=document.querySelector('#task-form');
 const clearBtn=document.querySelector(".btn");
+
+
+// Loading all event listeners
+loadAllEventListeners();
+
+function loadAllEventListeners(){
+    // Adding task event
+    form.addEventListener("submit",addTask);
+
+
+}
+
+// Defining addTask
+function addTask(e){
+    if(taskInput.value===""){
+        alert("Please enter an input.")
+    }
+
+    // Create li element
+    const li=document.createElement("li");
+    // Add class
+    li.className='collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create a new link element
+    const link=document.createElement('a');
+    // Add class
+    link.className="delete-item secondary-content";
+    // Create html icon
+    link.innerHTML='<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li); 
+
+    // Clear input field
+    taskInput.value="";
+
+
+    e.preventDefault();
+    
+
+}
