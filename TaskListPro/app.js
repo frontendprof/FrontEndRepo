19aecef7b458648039ab75c9e96cc29da8692b55
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-26 02:24:11 -0600

    Removing data from local storage

--- TaskListPro/app.js
+++ TaskListPro/app.js
@@ -108,7 +108,25 @@ function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         e.target.parentElement.parentElement.remove();
     }
+
+    removeFromLocalStorage(e.target.parentElement.parentElement);
     
+}
+
+// Remove from LS
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem("tasks")===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem("tasks"));
+    }
+    tasks.forEach(function(task,index){
+        if(taskItem.textContent===task){
+            tasks.splice(index,1);
+        }
+    });
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
 
 // Clear tasks
@@ -132,6 +150,6 @@ function filterTasks(e){
         }
 
     });
-    
+       
 
 }
