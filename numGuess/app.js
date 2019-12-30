98b1e6a6e1112942320578468757596999be0910
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-30 02:34:45 -0600

    Inserting validation upon project

--- numGuess/app.js
+++ numGuess/app.js
@@ -12,4 +12,26 @@ const minNum=document.querySelector(".min-num");
 const maxNum=document.querySelector(".max-num");
 let inpVal=document.querySelector("#guess-num");
 const subVal=document.querySelector("#sub-num");
-let msg=document.querySelector(".message");
+let conMsg=document.querySelector(".message");
+
+
+// Assign UI min and max values
+minNum.textContent=min;
+maxNum.textContent=max;
+
+// Event listener
+subVal.addEventListener('click',function(){
+    let guess=parseInt(inpVal.value);
+    console.log(guess);
+
+    // Validate
+    if(isNaN(guess) || minNum<1 ||maxNum>10){
+        setMsg(`Please enter a number between ${min} and ${max}`);
+    }
+    
+});
+
+function setMsg(msg){
+    conMsg.textContent=msg;
+
+}
