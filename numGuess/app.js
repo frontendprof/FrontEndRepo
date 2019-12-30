b3b1cbbfde5af14dd41950141f0047c75ccdd4c3
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-30 02:22:12 -0600

    Defining UI elements and game values'

---
+++ numGuess/app.js
@@ -0,0 +1,15 @@
+
+
+// Defining game values
+let min=1,
+    max=10,
+    winNum=8,
+    gLeft=3;
+
+// Defining UI Elements
+const game=document.querySelector("#game");
+const minNum=document.querySelector(".min-num");
+const maxNum=document.querySelector(".max-num");
+let inpVal=document.querySelector("#guess-num");
+const subVal=document.querySelector("#sub-num");
+let msg=document.querySelector(".message");
