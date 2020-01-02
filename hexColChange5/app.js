c8d92f84ded8ad5a0284bb7571c38dd0be6821a0
AbdulMalik <abdumaliksharipov@gmail.com>
2020-01-01 23:43:59 -0600

    Defining immediate invoked function expression

---
+++ hexColChange5/app.js
@@ -0,0 +1,18 @@
+
+
+// Defining immediate invoked function expression
+
+(function(){
+
+    const btn=document.querySelector(".btn");
+    const hexCol=document.querySelector(".hex-color");
+
+    btn.addEventListener("click",function(){
+        const hexP="#";
+        const hexVal=[1,2,3,4,5,6,"A","B","C","D","E","F"];
+
+        
+    })
+
+
+})();
