b95fcb5ef061eaf7c7d710f417308210a434586d
AbdulMalik <abdumaliksharipov@gmail.com>
2020-01-01 23:59:03 -0600

    hex and background change color project completion

--- hexColChange5/app.js
+++ hexColChange5/app.js
@@ -8,10 +8,19 @@
     const hexCol=document.querySelector(".hex-color");
 
     btn.addEventListener("click",function(){
-        const hexP="#";
+        let hexP="#";
         const hexVal=[1,2,3,4,5,6,"A","B","C","D","E","F"];
 
-        
+
+        for(let i=0; i<6; i++){
+            let randVal=Math.floor(Math.random()*hexVal.length);
+            hexP += hexVal[randVal];
+            
+        }
+        hexCol.textContent=hexP;
+        document.body.style.backgroundColor=hexP;
+
+
     })
 
 
