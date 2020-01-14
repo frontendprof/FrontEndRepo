7e76efa18ad1388aa30f787586acab38e1c57832
AbdulMalik <abdumaliksharipov@gmail.com>
2020-01-14 02:19:18 -0600

    With small refinements

--- backgSlider/app.js
+++ backgSlider/app.js
@@ -10,7 +10,11 @@
         "cB-1.jpeg",
         "cB-2.jpeg",
         "cB-3.jpeg",
-        "cB-4.jpeg"
+        "cB-4.jpeg",
+        "cB-5.jpeg",
+        "cB-6.jpeg",
+        "cB-7.jpeg",
+        "cB-8.jpeg"
 
     ];
 
@@ -27,7 +31,16 @@
                     counter=pics.length-1;
                 }
                 document.querySelector(".img-cont").style.backgroundImage=`url("img/${pics[counter]}")`;                
-            }        
+            }    
+            
+            
+            if(e.target.classList.contains("btn-right")){
+                counter++;
+                if(counter>pics.length-1){
+                    counter=0;
+                }
+                document.querySelector(".img-cont").style.backgroundImage=`url("img/${pics[counter]}")`;                
+            }
             
             
 
