96688ecc2871ef15b1808df7d7b26e35c88e2307
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-17 19:19:22 +0500

    Defining callbacks

--- timerSVG/index.js
+++ timerSVG/index.js
@@ -1,17 +1,25 @@
 
 
 class Timer{
-    constructor(durationInput,startBtn,pauseBtn){
+    constructor(durationInput,startBtn,pauseBtn,callbacks){
         this.durationInput=durationInput;
         this.startBtn=startBtn;
         this.pauseBtn=pauseBtn;
 
         this.startBtn.addEventListener("click",this.start);
         this.pauseBtn.addEventListener("click",this.pause);
+
+        if(callbacks){
+            this.onStart=callbacks.onStart;
+        }
     
     }
 
     start=()=>{
+
+        if(this.onStart){
+            this.onStart();
+        }
 
         this.tick();
         this.interval=setInterval(this.tick,1000);
@@ -23,8 +31,13 @@ class Timer{
     }
 
     tick=()=>{
-        const timeRemaining=this.timeRemaining;
-        this.timeRemaining=timeRemaining-1;
+
+        if(this.timeRemaining<=0){
+            this.pause();
+        }else{
+            this.timeRemaining=this.timeRemaining-1;
+        }
+        
         
     }
 
@@ -42,4 +55,13 @@ const durationInput=document.querySelector("#duration");
 const startBtn=document.querySelector("#start");
 const pauseBtn=document.querySelector("#pause");
 
-const timer=new Timer(durationInput,startBtn,pauseBtn);
+const timer=new Timer(durationInput,startBtn,pauseBtn,{
+    onStart(){
+        console.log("Timer started")
+        
+    },
+
+    onTick(){},
+
+    onComplete(){}
+});
