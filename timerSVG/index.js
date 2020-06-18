4d7dd553c9649070507b2edd265b91ddc23ab586
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-18 19:26:05 +0500

    Extracting timer code

--- timerSVG/index.js
+++ timerSVG/index.js
@@ -1,55 +1,5 @@
 
 
-class Timer{
-    constructor(durationInput,startBtn,pauseBtn,callbacks){
-        this.durationInput=durationInput;
-        this.startBtn=startBtn;
-        this.pauseBtn=pauseBtn;
-
-        this.startBtn.addEventListener("click",this.start);
-        this.pauseBtn.addEventListener("click",this.pause);
-
-        if(callbacks){
-            this.onStart=callbacks.onStart;
-        }
-    
-    }
-
-    start=()=>{
-
-        if(this.onStart){
-            this.onStart();
-        }
-
-        this.tick();
-        this.interval=setInterval(this.tick,1000);
-        
-    }
-    pause=()=>{
-        clearInterval(this.interval);
-        
-    }
-
-    tick=()=>{
-
-        if(this.timeRemaining<=0){
-            this.pause();
-        }else{
-            this.timeRemaining=this.timeRemaining-1;
-        }
-        
-        
-    }
-
-    get timeRemaining(){
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time){
-        this.durationInput.value=time;
-    }
-}
-
 
 const durationInput=document.querySelector("#duration");
 const startBtn=document.querySelector("#start");
@@ -61,7 +11,13 @@ const timer=new Timer(durationInput,startBtn,pauseBtn,{
         
     },
 
-    onTick(){},
+    onTick(){
+        console.log("Timer just ticked down");
+        
+    },
 
-    onComplete(){}
+    onComplete(){
+        console.log("Timer is complete");
+        
+    }
 });
