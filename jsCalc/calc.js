fe9bf8bbb96a093b5754b1177605f63fd34c128d
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-25 19:54:38 +0500

    Defining all functions

--- jsCalc/calc.js
+++ jsCalc/calc.js
@@ -8,3 +8,38 @@ let mathOperator="";
 let decimalClicked=false;
 let valMemStored="";
 
+
+function numButPress(num){
+    
+}
+
+function mathButPress(operator){
+    
+}
+
+function equalButPress(num){
+    
+}
+
+function clearButPress(){
+    prevVal="";
+    newVal="";
+    resultVal="";
+    mathOperator="";
+    decimalClicked=false;
+    document.querySelector("#entry").value="0";
+    
+}
+
+function copyButPress(num){
+    valMemStored=document.querySelector("#entry").value;
+    
+}
+
+function pasteButPress(num){
+    if(valMemStored){
+        document.querySelector("#entry").value="0"=valMemStored;
+        newVal=valMemStored;
+    }
+    
+}
