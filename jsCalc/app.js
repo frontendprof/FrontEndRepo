0b5433e6cfe8eb3d18f6996cd468afde8fae18ae
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-25 23:22:49 +0500

    Init final

---
+++ jsCalc/app.js
@@ -0,0 +1,91 @@
+
+// Defining variables
+
+let prevVal="";
+let newVal="";
+let resultVal="";
+let mathOperator="";
+let decimalClicked=false;
+let valMemStored="";
+
+
+function numButPress(num){
+    if(resultVal){
+        newVal=num;
+        resultVal="";
+    }else{
+        if(num==="."){
+            if(decimalClicked!=true){
+                newVal+=num;
+                decimalClicked=true;
+            }
+        }else{
+            newVal+=num;
+        }
+    }
+    document.querySelector("#entry").value=newVal;
+    
+}
+
+function mathButPress(operator){
+    if(!resultVal){
+        prevVal=newVal;
+    }else{
+        prevVal=resultVal;
+    }
+    newVal="";
+    decimalClicked=false;
+    mathOperator=operator;
+    resultVal="";
+    document.querySelector("#entry").value="";
+
+    
+}
+
+function equalButPress(num){
+    decimalClicked=false;
+    prevVal=parseFloat(prevVal);
+    newVal=parseFloat(newVal);
+
+    switch(mathOperator){
+        case "+":
+            resultVal=prevVal+newVal;
+            break;
+        case "-":
+            resultVal=prevVal-newVal;
+            break;
+        case "*":
+            resultVal=prevVal*newVal;
+            break;
+        case "/":
+            resultVal=prevVal/newVal;
+            break;
+        default:
+            resultVal=newVal;
+    }
+    prevVal=resultVal;
+    document.querySelector("#entry").value=resultVal;
+}
+
+function clearButPress(){
+    prevVal="";
+    newVal="";
+    resultVal="";
+    mathOperator="";
+    decimalClicked=false;
+    document.querySelector("#entry").value="0";
+    
+}
+
+function copyButPress(num){
+    valMemStored=document.querySelector("#entry").value;
+    
+}
+
+function pasteButPress(num){
+    if(valMemStored){
+        document.querySelector("#entry").value=valMemStored;
+        newVal=valMemStored;
+    }
+    
+}
